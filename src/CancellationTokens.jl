deae1ac7208d6163819b6a6a5d75cd84a39ff8ae
David Anthoff <anthoff@berkeley.edu>
2021-07-23 18:42:56 -0700

    Add support for registration of callbacks

--- src/CancellationTokens.jl
+++ src/CancellationTokens.jl
@@ -1,6 +1,13 @@
 module CancellationTokens
 
-export CancellationTokenSource, get_token, is_cancellation_requested, cancel, OperationCanceledException
+export
+    CancellationTokenSource,
+    get_token,
+    is_cancellation_requested,
+    cancel,
+    OperationCanceledException,
+    register,
+    register_closable
 
 include("event.jl")
 
@@ -9,10 +16,11 @@ include("event.jl")
 mutable struct CancellationTokenSource
     _state::CancellationTokenSourceStates
     _timer::Union{Nothing,Timer}
-    _kernel_event::Union{Nothing,Event} # TODO Event is Julia > 1.1, make it work on 1.0
+    _kernel_event::Union{Nothing,Event}
+    _registrations::Union{Nothing,Vector{Any}}
 
     function CancellationTokenSource()
-        return new(NotCanceledState, nothing, nothing)
+        return new(NotCanceledState, nothing, nothing, nothing)
     end
 end
 
@@ -32,16 +40,40 @@ function _internal_notify(x::CancellationTokenSource)
 
         if x._timer!==nothing
             close(x._timer)
-            x._timer = nothing            
+            x._timer = nothing
         end
-    
+
         if x._kernel_event!==nothing
             notify(x._kernel_event)
             x._kernel_event = nothing
         end
 
         x._state = NotifyingCompleteState
+
+        _execute_callbacks(x)
     end
+end
+
+function _execute_callbacks(cts::CancellationTokenSource, throw_on_first_exception::Bool)
+    exceptions = []
+
+    for callback in cts._registrations
+        try
+            callback()
+        catch err
+            if throw_on_first_exception
+                rethrow(err)
+            else
+                push!(exceptions, err)
+            end
+        end
+    end
+
+    if length(exceptions) > 0
+        throw(CompositeException(exceptions))
+    end
+
+    return nothing
 end
 
 function cancel(x::CancellationTokenSource)
@@ -58,6 +90,23 @@ function _waithandle(x::CancellationTokenSource)
     end
 
     return x._kernel_event
+end
+
+function register(cts::CancellationTokenSource, f)
+    if !is_cancellation_requested(cts)
+        if cts._registrations===nothing
+            cts._registrations = []
+        end
+
+        push!(cts._registrations, f)
+    else
+        f()
+        return nothing
+    end
+end
+
+function register_closable(cts::CancellationTokenSource, x)
+    register(cts, ()->close(x))
 end
 
 # CancellationToken
@@ -79,6 +128,10 @@ function Base.wait(x::CancellationToken)
         wait(_waithandle(x))
     end
 end
+
+register(ct::CancellationToken, f) = register(ct._source, f)
+
+register_closable(ct:CancellationToken, x) = register_closable(ct._source, x)
 
 # OperationCanceledException
 
